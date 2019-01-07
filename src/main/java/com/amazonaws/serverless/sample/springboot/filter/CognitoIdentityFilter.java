cc0fc1610faa07b843ccd79f9400e50ef3d10ded
Khalid <khalid.mtwaly@gmail.com>
2019-01-08 01:02:26 +0200

    bug fix

--- src/main/java/com/amazonaws/serverless/sample/springboot/filter/CognitoIdentityFilter.java
+++ src/main/java/com/amazonaws/serverless/sample/springboot/filter/CognitoIdentityFilter.java
@@ -2,7 +2,8 @@ package com.amazonaws.serverless.sample.springboot.filter;
 
 
 import com.amazonaws.serverless.proxy.RequestReader;
-import com.amazonaws.serverless.proxy.model.ApiGatewayRequestContext;
+import com.amazonaws.serverless.proxy.model.ApiGatewayAuthorizerContext;
+import com.amazonaws.serverless.proxy.model.ApiGatewayRequestIdentity;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -42,17 +43,17 @@ public class CognitoIdentityFilter implements Filter {
             log.warn("API Gateway context is null");
             filterChain.doFilter(servletRequest, servletResponse);
         }
-        if (!ApiGatewayRequestContext.class.isAssignableFrom(apiGwContext.getClass())) {
+        if (!ApiGatewayRequestIdentity.class.isAssignableFrom(apiGwContext.getClass())) {
             log.warn("API Gateway context object is not of valid type");
             filterChain.doFilter(servletRequest, servletResponse);
         }
 
-        ApiGatewayRequestContext ctx = (ApiGatewayRequestContext)apiGwContext;
-        if (ctx.getIdentity() == null) {
+        ApiGatewayRequestIdentity ctx = (ApiGatewayRequestIdentity)apiGwContext;
+        if (ctx.getCognitoIdentityId() == null) {
             log.warn("Identity context is null");
             filterChain.doFilter(servletRequest, servletResponse);
         }
-        String cognitoIdentityId = ctx.getIdentity().getCognitoIdentityId();
+        String cognitoIdentityId = ctx.getCognitoIdentityId();
         if (cognitoIdentityId == null || "".equals(cognitoIdentityId.trim())) {
             log.warn("Cognito identity id in request is null");
         }
